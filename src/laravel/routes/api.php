6f7548d7825a694dfd0ea2eb5f8aad5b37103718
Shahriar Farzam <sh.farzam@hotmail.com>
2023-04-20 14:24:44 +0200

    set Routes

--- src/laravel/routes/api.php
+++ src/laravel/routes/api.php
@@ -20,17 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Create article
-Route::post('/articles', [ArticleController::class, 'create']);
+Route::post('/articles', [ArticleController::class, 'store']);
 
 // Edit article
 Route::put('/articles/{id}', [ArticleController::class, 'update']);
 
 // List articles
-Route::get('/articles', [ArticleController::class, 'list']);
+Route::get('/articles', [ArticleController::class, 'index']);
 
 // View article
-Route::get('/articles/{id}', [ArticleController::class, 'view']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
 
 // Delete article
 Route::delete('/articles/{id}', [ArticleController::class, 'delete']);
-
